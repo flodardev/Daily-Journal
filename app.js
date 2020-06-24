f846228f0f3c37f6085fe29ada7700e286268089
flodardev <63950579+flodardev@users.noreply.github.com>
2020-06-24 16:42:30 +0800

    Ready for Heroku

--- app.js
+++ app.js
@@ -80,6 +80,8 @@ app.get("/posts/:title", function(req, res) {
   res.render("post", {post: post404})
 })
 
-app.listen(3000, function() {
+let port = process.env.PORT || 3000
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
